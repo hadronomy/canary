de6764e8260d7f21c2688fba9ca409aedec3c672
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2026-01-18 04:32:22 +0000

    feat: update tui

--- apps/tui/src/index.tsx
+++ apps/tui/src/index.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <box alignItems="center" justifyContent="center" flexGrow={1}>
       <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
+        <ascii-font font="tiny" text="Canary" />
+        <text attributes={TextAttributes.DIM}>What will you search?</text>
       </box>
     </box>
   );
