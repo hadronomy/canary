3fa96650dbe2e9070b15c49fdb4488935123c9ae
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2026-01-18 03:36:07 +0000

    feat: first commit :star2:

---
+++ apps/server/src/index.ts
@@ -0,0 +1,15 @@
+import { env } from "@canary/env/server";
+import { cors } from "@elysiajs/cors";
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .use(
+    cors({
+      origin: env.CORS_ORIGIN,
+      methods: ["GET", "POST", "OPTIONS"],
+    }),
+  )
+  .get("/", () => "OK")
+  .listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
