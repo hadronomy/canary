dccfc810a7147dd37703811fb9845c8da58dfc95
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2026-03-13 14:25:17 +0000

    feat(docs): clean the docs up a bit

--- apps/fumadocs/src/app/layout.tsx
+++ apps/fumadocs/src/app/layout.tsx
@@ -1,15 +1,21 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import { GeistPixelGrid } from 'geist/font/pixel';
 import { Inter } from 'next/font/google';
 
 import './global.css';
 
 const inter = Inter({
   subsets: ['latin'],
+  variable: '--font-inter',
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${inter.variable} ${GeistPixelGrid.variable}`}
+      suppressHydrationWarning
+    >
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
